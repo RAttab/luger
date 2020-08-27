42f1e137db67ebb239f31e6f778da2272e970ae8
Richard Kallos <rkallos@gmail.com>
2020-08-27 13:27:38 -0400

    Replace 'undefined' max_msg_len with 'infinity'

--- src/luger.erl
+++ src/luger.erl
@@ -110,7 +110,7 @@ debug(Channel, Format, Args) ->
           syslog_udp_host   = undefined   :: undefined | string(),
           syslog_udp_port   = undefined   :: undefined | integer(),
           syslog_udp_facility = undefined :: undefined | integer(),
-          max_msg_len                     :: undefined | integer()
+          max_msg_len                     :: infinity | integer()
          }).
 
 init([#config{app = AppName, host = HostName, max_msg_len = MaxLen}, SinkConfig, SingleLine, ThrottleThreshold]) ->
